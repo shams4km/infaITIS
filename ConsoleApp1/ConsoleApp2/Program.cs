170c8c136b0edb0a26e707df83a8645072a0a99a
shams4km <shams4km@gmail.com>
2023-12-06 17:07:53 +0300

    matrix

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Dictionary<string, int> frequencyDict = new Dictionary<string, int>();
+Random r = new Random();
+foreach (string line in File.ReadAllLines("C:/input/voynaimir.txt"))
+{
+    var words = line.ToLower().Split(new char[] { ',', '.', '!', ':', '-', ' ' });
+    foreach (var word in words)
+    {
+        if (word.Length > 0 && char.IsLetter(word[0]))
+        {
+            if (frequencyDict.Keys.Contains(word)) frequencyDict[word]++;
+            else frequencyDict[word] = 1;
+        }
+    }
+}
+foreach (var k in frequencyDict.Keys)
+{
+    Console.WriteLine($"{k} - {frequencyDict[k]}");
+}
+
+
